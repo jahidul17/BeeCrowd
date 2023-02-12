78b6a915fa6816f8e0def2a631c29b21cb408514
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-02-12 23:43:46 +0600

    12/02/2023 solving this programm

--- Beginner/1044 Multiples/main.c
+++ Beginner/1044 Multiples/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if((b%a)==0 ||  (a%b)==0){
+        printf("Sao Multiplos\n");
+    }
+    else{
+        printf("Nao sao Multiplos\n");
+    }
     return 0;
 }
---
+++ Beginner/1045 Triangle Types/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    double a,b,c;
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    if((a>0) && (b>0) && (c>0))
+    {
+        if(a>=b+c)
+        {
+            printf("NAO FORMA TRIANGULO\n");
+        }
+        if((a*a)==(b*b)+(c*c))
+        {
+            printf("TRIANGULO RETANGULO\n");
+        }
+        if((a*a)>(b*b)+(c*c))
+        {
+            printf("TRIANGULO OBTUSANGULO\n");
+        }
+        if((a*a)<(b*b)+(c*c))
+        {
+            printf("TRIANGULO ACUTANGULO\n");
+        }
+        if(a==b==c)
+        {
+            printf("TRIANGULO EQUILATERO\n");
+        }
+        if((a==b)||(b==c)||(c==a))
+        {
+            printf("TRIANGULO ISOSCELES\n");
+        }
+
+    }
+
+    return 0;
+}
