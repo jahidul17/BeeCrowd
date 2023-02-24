4d13aaa07806f3ddc20c1c4a3aa5494b328a8f31
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-02-24 23:37:39 +0600

    23/02/2023 solving this programm

---
+++ Beginner/1070 Six Odd Numbers/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+
+    if(num>0){
+        if((num%2)!=0){
+            printf("%d\n",num);
+
+            }
+        }
+
+
+    return 0;
+}
