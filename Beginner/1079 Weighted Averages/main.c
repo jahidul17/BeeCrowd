b2638514d668ab481bf075bebc7f704a21c7eaeb
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-03-02 23:51:20 +0600

    02/03/2023 I can't solve this program.

---
+++ Beginner/1079 Weighted Averages/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    //scanf("%d",&n);
+    double num[3],sum=0;
+
+    for(i=0;i<3;i++){
+        scanf("%lf",&num[3]);
+    }
+
+    for(i=0;i<3;i++){
+        printf("%lf\n",num[i]);
+    }
+
+
+
+    return 0;
+}
