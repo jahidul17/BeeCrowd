cec5afd4740c1fcb8d6ef6e3c8b15a496227148b
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-03-19 20:48:48 +0600

    19/03/2023 solved this program.

--- Beginner/1080 Highest and Position/main.c
+++ Beginner/1080 Highest and Position/main.c
@@ -1,22 +1,37 @@
+/*1080	Highest and Position*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    int num[5],i;
+    int largest,element[100],i;
 
 
-    for(i=0; i<5; i++)
+    for(i=0; i<100; i++)
     {
-        scanf("%d",&num[i]);
+        scanf("%d",&element[i]);
     }
 
-    rem[0]=num[0];
+    largest=element[0];
 
-    for(i=0; i<5; i++)
+    for(i=1; i<100; i++)
     {
-        if(rem)
-        printf("%d\n",num[i]);
+        if(element[i]>largest)
+        {
+            largest=element[i];
+
+        }
+
+    }
+
+    printf("%d\n",largest);
+
+    for(i=0; i<100; i++)
+    {
+        if(element[i]==largest)
+        {
+            printf("%d\n",i+1);
+        }
     }
 
     return 0;
