bea54c86e8145ef9db568b52ceaa6a0325b86685
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-03-20 22:59:25 +0600

    20/03/2023 I solved this program.

---
+++ Beginner/1095 Sequence IJ 1/main.c
@@ -0,0 +1,13 @@
+/*1095 Sequence IJ 1*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(j=60,i=1;j>=0,i<=37;i=i+3,j-=5){
+        printf("I=%d J=%d\n",i,j);
+
+    }
+    return 0;
+}
