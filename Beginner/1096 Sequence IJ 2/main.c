1df1b3d6f0606594d0959f3628ce1f7baef1b45c
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-03-21 22:28:04 +0600

    21/03/2023 solved this program.

---
+++ Beginner/1096 Sequence IJ 2/main.c
@@ -0,0 +1,16 @@
+/*1096		Sequence IJ 2*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    for(i=1;i<=9;i+=2){
+        printf("I=%d J=7\nI=%d J=6\nI=%d J=5\n",i,i,i);
+
+    }
+
+
+    return 0;
+}
