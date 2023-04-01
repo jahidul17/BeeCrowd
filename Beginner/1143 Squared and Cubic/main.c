64c51a07a57d3236236050331c152bd52816c710
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-01 16:00:07 +0600

    1/04/2023 solved this program.

---
+++ Beginner/1143 Squared and Cubic/main.c
@@ -0,0 +1,16 @@
+/* 1143		Squared and Cubic */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++){
+        printf("%d %d %d\n",i,i*i,i*i*i);
+
+    }
+
+    return 0;
+}
