099095981eca3848b1597ee68015842ca4cebc8b
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-02 23:11:42 +0600

    It's Done

---
+++ Beginner/1151 Easy Fibonacci/main.c
@@ -0,0 +1,25 @@
+/* 1151		Easy Fibonacci */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n,fibo,fibo_1,fibo_2;
+    scanf("%d",&n);
+
+    fibo_1=0;
+    fibo_2=1;
+    printf("%d ",fibo_1);
+    printf("%d",fibo_2);
+
+    for(i=2;i<n;i++){
+        fibo=fibo_1+fibo_2;
+         printf(" %d",fibo);
+
+        fibo_1=fibo_2;
+        fibo_2=fibo;
+
+    }
+
+    return 0;
+}
