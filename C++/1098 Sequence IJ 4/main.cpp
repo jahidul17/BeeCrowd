7bca76ef6d0179b66e2103a969af66c95399f754
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-08 20:31:17 +0600

    It's Done.

---
+++ C++/1098 Sequence IJ 4/main.cpp
@@ -0,0 +1,17 @@
+/* 1098	Sequence IJ 4 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double i;
+
+    for(i=0;i<=2;i+=0.2){
+        cout<<"I="<<i<<" "<<"J="<<i+1<<endl;
+        cout<<"I="<<i<<" "<<"J="<<i+2<<endl;
+        cout<<"I="<<i<<" "<<"J="<<i+3<<endl;
+    }
+
+    return 0;
+}
