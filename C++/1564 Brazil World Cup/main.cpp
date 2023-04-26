461288056278c86aed07ca546af2e27ca604861c
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-26 17:17:34 +0600

    It's Done.

---
+++ C++/1564 Brazil World Cup/main.cpp
@@ -0,0 +1,21 @@
+/* 1564		Brazil World Cup */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    while(scanf("%d",&n) != EOF){
+        if(n==0){
+            cout<<"vai ter copa!"<<endl;
+        }
+        else{
+            cout<<"vai ter duas!"<<endl;
+        }
+    }
+
+
+    return 0;
+}
