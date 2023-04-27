f82e1fb01ac5e566ad25fa5a132b54d4f8ea21a0
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-27 22:42:20 +0600

    It's Done.

---
+++ C++/1759 Ho Ho Ho/main.cpp
@@ -0,0 +1,22 @@
+/* 1759		Ho Ho Ho */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+        if(i==n){
+            cout<<"Ho!\n";
+        }
+        else{
+            cout<<"Ho ";
+        }
+    }
+
+
+    return 0;
+}
