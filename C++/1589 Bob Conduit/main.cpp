066c56d27329c6b70b9a250bda0375bd877029fd
Jahidul Islam <jahidulislam.khan1234@gmail.com>
2023-04-27 22:33:25 +0600

    It's Done.

---
+++ C++/1589 Bob Conduit/main.cpp
@@ -0,0 +1,21 @@
+/* 1589		Bob Conduit */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,t,r1,r2,r3;
+    cin>>t;
+
+    for(i=1;i<=t;i++){
+        cin>>r1>>r2;
+
+        r3=r1+r2;
+
+        cout<<r3<<endl;
+    }
+
+
+    return 0;
+}
